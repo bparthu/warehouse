2d2ff1645b9559f63d7e4739853fff4a2110840f
Parthasarathy B <b.paarthu@gmail.com>
2021-02-10 11:33:12 +0100

    The edge case that cost me the interview

--- apps/seeder/src/sql/createTableInventory.sql
+++ apps/seeder/src/sql/createTableInventory.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS `Inventory` (
     `name` text,
     `stock` int,
     primary key(`id`),
-    CONSTRAINT `stock_column_positive` CHECK (`stock` > 0)
+    CONSTRAINT `stock_column_positive` CHECK (`stock` >= 0)
 ) ENGINE=INNODB;
